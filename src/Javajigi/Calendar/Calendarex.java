334b3df28bc4de666ca8a2c3c859f065630d2052
Jihun Han <hjh_3229@naver.com>
2023-04-26 17:40:36 +0900

    good example 3 but I like Making_3 ver

--- src/Javajigi/Calendar/Calendarex.java
+++ src/Javajigi/Calendar/Calendarex.java
@@ -2,23 +2,26 @@ package Javajigi.Calendar;
 
 public class Calendarex {
 
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public boolean isLeepYear(int year) {
         return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 
     public int getMaxDaysOfMonth(int year, int month) {
         if (isLeepYear(year)) {
-            return LEAP_MAX_DAYS[month - 1];
-        } else return MAX_DAYS[month - 1];
+            return LEAP_MAX_DAYS[month];
+        } else return MAX_DAYS[month];
     }
 
 
-    public void printCalendar(int year, int month, int weekday) {
-        System.out.printf("   <<%4d년%3d월>>\n", year, month);
+    public void printCalendar(int year, int month) {
+        System.out.printf("   <<%d년 %d월>>\n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("-----------------------");
+
+        //get weekday automatically
+        int weekday = getWeekDay(year, month);
 
         for (int i = 0; i < weekday; i++) {
             System.out.print("   ");
@@ -33,5 +36,24 @@ public class Calendarex {
         }
 
         System.out.println();
+    }
+
+    private int getWeekDay(int year, int month) {
+        int syear = 1970;
+        final int STANDARD_WEEKDAY = 4;
+
+        int count = 0;
+
+        for (int i = syear; i < year; i++) {
+            int delta = isLeepYear(i) ? 366 : 365;
+            count += delta;
+        }
+
+        for (int i = 1; i < month; i++) {
+            int delta = getMaxDaysOfMonth(year, i);
+            count += delta;
+        }
+
+        return (count + STANDARD_WEEKDAY)%7;
     }
 }
--- src/Javajigi/Calendar/Promptex.java
+++ src/Javajigi/Calendar/Promptex.java
@@ -2,27 +2,6 @@ package Javajigi.Calendar;
 import java.util.Scanner;
 public class Promptex {
 
-    public int parseDay(String week) {
-        switch (week) {
-            case "su":
-                return 0;
-            case "mo":
-                return 1;
-            case "tu":
-                return 2;
-            case "we":
-                return 3;
-            case "th":
-                return 4;
-            case "fr":
-                return 5;
-            case "sa":
-                return 6;
-            default:
-                return -1;
-        }
-    }
-
     public void rumPrompt() {
         Scanner scanner = new Scanner(System.in);
         Calendarex cal = new Calendarex();
@@ -40,15 +19,8 @@ public class Promptex {
                 System.out.println("잘못된 입력입니다.");
                 break;
             }
-            System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, we, th, fr, sa).");
-            String str_weekday = scanner.next();
-            int weekday = parseDay(str_weekday);
-            if (weekday == -1) {
-                System.out.println("잘못된 입력입니다.");
-                break;
-            }
 
-            cal.printCalendar(year, month, weekday);
+            cal.printCalendar(year, month);
         }
 
         System.out.println("반복이 종료되었습니다.");
