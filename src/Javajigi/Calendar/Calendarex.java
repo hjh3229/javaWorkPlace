6bbe71cca6a9dd20f209f7b4ca628011eae3692b
Jihun Han <hjh_3229@naver.com>
2023-04-26 15:35:59 +0900

    Making myself 2

--- src/Javajigi/Calendar/Calendarex.java
+++ src/Javajigi/Calendar/Calendarex.java
@@ -14,24 +14,23 @@ public class Calendarex {
         } else return MAX_DAYS[month - 1];
     }
 
-    public void printCalendar(int year, int month) {
+    public void printCalendar(int year, int month, int weekday) {
         System.out.printf("   <<%4d년%3d월>>\n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("-----------------------");
 
+        for (int i = 0; i < weekday; i++) {
+            System.out.print("   ");
+        }
         int maxday = getMaxDaysOfMonth(year, month);
 
         for(int i = 1; i <= maxday; i++) {
             System.out.printf("%3d", i);
-            if (i%7 == 0) {
+            if ((i+weekday)%7 == 0) {
                 System.out.println();
             }
         }
 
         System.out.println();
-//        System.out.println(" 1  2  3  4  5  6  7");
-//        System.out.println(" 8  9 10 11 12 13 14");
-//        System.out.println("15 16 17 18 19 20 21");
-//        System.out.println("22 23 24 25 26 27 28");
     }
 }
--- src/Javajigi/Calendar/Promptex.java
+++ src/Javajigi/Calendar/Promptex.java
@@ -2,19 +2,42 @@ package Javajigi.Calendar;
 import java.util.Scanner;
 public class Promptex {
 
-    private final static String PROMPT="cal> ";
+    public int parseDay(String week) {
+        switch (week) {
+            case "su":
+                return 0;
+            case "mo":
+                return 1;
+            case "tu":
+                return 2;
+            case "we":
+                return 3;
+            case "th":
+                return 4;
+            case "fr":
+                return 5;
+            case "sa":
+                return 6;
+            default:
+                return 0;
+        }
+    }
 
     public void rumPrompt() {
         Scanner scanner = new Scanner(System.in);
         Calendarex cal = new Calendarex();
 
         while (true) {
-            System.out.println("년도를 입력해 주세요");
-            System.out.print(PROMPT);
+            System.out.println("년도를 입력해 주세요.");
+            System.out.print("YEAR> ");
             int year = scanner.nextInt();
-            System.out.println("달을 입력해 주세요");
-            System.out.print(PROMPT);
+            System.out.println("달을 입력해 주세요.");
+            System.out.print("MONTH> ");
             int month = scanner.nextInt();
+            System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, we, th, fr, sa).");
+            String str_weekday = scanner.next();
+            int weekday = parseDay(str_weekday);
+            System.out.println(str_weekday + ", " + weekday);
             if (month == -1) {
                 break;
             }
@@ -22,7 +45,7 @@ public class Promptex {
                 continue;
             }
 
-            cal.printCalendar(year, month);
+            cal.printCalendar(year, month, weekday);
         }
 
         System.out.println("반복이 종료되었습니다.");
