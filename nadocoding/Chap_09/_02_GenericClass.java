5966ea33ebcc6367b6784efc7b508bfb4ba3fa4d
Han JiHun <hjh_3229@naver.com>
2023-06-08 16:48:25 +0900

    commit test

--- nadocoding/Chap_09/_02_GenericClass.java
+++ nadocoding/Chap_09/_02_GenericClass.java
@@ -5,7 +5,7 @@ import nadocoding.Chap_09.coffee.CoffeeByNumber;
 public class _02_GenericClass {
     public static void main(String[] args) {
         // 제네릭 클래스
-        CoffeeByNumber c1 = new CoffeeByNumber(32);
+        CoffeeByNumber c1 = new CoffeeByNumber(33);
         c1.ready();
     }
 }
